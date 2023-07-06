25c532e36ac30e84e17df4aa2b8c28f00851dc1c
Oscar Coghlan <oscar.coghlan@gmail.com>
2023-07-06 20:32:07 +1000

    getting there

--- template/content.php
+++ template/content.php
@@ -1,50 +1,86 @@
-	<div class="bodycontent">
-		<div class="firstpage">
-			<div class="title">
-				<p id="maintitle">Oscar Coghlan</p>
-				<p id="subsubtitle">Software engineering student with a passion for learning</p>
-				<a href="#About" class="buttonStyle">learn more</a>
-				<!--<p class="pb15vh"> </p>-->
-			</div>
+<div class="bodycontent">
+	<div class="firstpage">
+		<div class="title">
+			<p id="maintitle">Oscar Coghlan</p>
+			<p id="subsubtitle">Software engineering student with a passion for learning</p>
+			<a href="#About" class="buttonStyle">learn more</a>
+			<!--<p class="pb15vh"> </p>-->
 		</div>
-	<div class="divwhitespace78" id="About"> &nbsp; </div>
-		<span class="contentBox">
-			<h2>About Me:</h2>
-			<br/>
-(CHATGPT)
-I'm Oscar, a dedicated software engineering student focused on delivering innovative solutions. On this portfolio website, I share my skills, projects, and experiences.
-
-<br>
-<br>
-From an early age, I've been intrigued by the possibilities of software engineering. I find fulfillment in creating elegant and efficient solutions to complex challenges, combining creativity and problem-solving. As a student, I actively pursue opportunities for growth and contribution in this ever-evolving field.
-
-<br>
-<br>
-Currently pursuing a degree in software engineering, I have developed a strong foundation in programming languages, data structures, algorithms, and software design principles. Proficient in Java, Python, C++, C, C#, PHP, and JavaScript, I'm consistently curious about exploring new technologies and frameworks.
-
-<br>
-<br>
-Beyond academia, I thrive in collaborative environments, valuing effective communication and a positive impact. Approachable and enthusiastic, I appreciate diverse perspectives and eagerly contribute my own ideas. I am actively seeking meaningful opportunities to apply my skills, expand my expertise as a software engineer, and drive positive change.
-
-<br>
-<br>
-Thank you for visiting my portfolio website. Feel free to explore my projects and accomplishments. If you have any inquiries or collaboration proposals, please don't hesitate to reach out. I look forward to connecting with you!
-			<br/>
-			<br/>
-			<div class="divwhitespace78" id="Projects"> &nbsp; </div>
-			<div class="portfolioentry">
-				<h2>Projects:</h2>
-				<img class="portfolioimage" src="/img/htree.png" alt="Image of coways game of life generation using the H-tree rule set">
-				<h3><a href="https://github.com/oscarcoghlan/automata-gl">automata-gl</a></h3>
-				<p>Conway's Game of Life is a classic cellular automaton that exhibits fascinating emergent behavior. In this coding project, I have created a simulation of Conway's Game of Life using C++ for calculation and OpenGL for rendering the world.</p>
-				<span>
-					<a href="https://github.com/oscarcoghlan/automata-gl">&nbsp; > &nbsp; View project on GitHub</a> <br> <br>
-					<a href="/res/automata-gl.exe">&nbsp; > &nbsp; Download standalone Windows PE binary</a>
-				</span>
-<br><br><br><br>
-
-</div>
-			</div>
-		</span>
 	</div>
+	<div class="divwhitespace78" id="About"> &nbsp; </div>
+	<span class="contentBox">
+		<h2>About Me:</h2>
+		<br/>
+		I'm Oscar, a dedicated software engineering student focused on delivering innovative solutions. On this portfolio website, I share my skills, projects, and experiences.
+
+		<br>
+		<br>
+		From an early age, I've been intrigued by the possibilities of software engineering. I find fulfillment in creating elegant and efficient solutions to complex challenges, combining creativity and problem-solving. As a student, I actively pursue opportunities for growth and contribution in this ever-evolving field.
+
+		<br>
+		<br>
+		I am currently pursuing a degree in software engineering, and have developed a strong foundation in programming languages, data structures, algorithms, and software design principles. Proficient in Java, Python, C++, C, C#, PHP, and JavaScript, I'm consistently curious about exploring new technologies and frameworks.
+
+		<br>
+		<br>
+		Beyond academia, I thrive in collaborative environments, valuing effective communication and a positive impact. Approachable and enthusiastic, I appreciate diverse perspectives and eagerly contribute my own ideas. I am actively seeking meaningful opportunities to apply my skills, expand my expertise as a software engineer.
+
+		<br>
+		<br>
+		Thank you for visiting my portfolio website. Feel free to explore my projects and accomplishments. If you have any inquiries or collaboration proposals, please don't hesitate to reach out via email at <a href=mailto:oscar.coghlan@gmail.com><u>oscar.coghlan@gmail.com</u></a>. I look forward to hearing from you.
+		<br/>
+		<br/>
+		<div class="divwhitespace78" id="Projects"> &nbsp; </div>
+		<div class="portfolioentry">
+			<h2 class="h2title">Projects:</h2>
+			<video class="portfolioimage" controls loop>
+				<source src="/media/golprev.mp4" type="video/mp4">
+				<p>Preview of maze cellular automata simulation</p>
+				Your browser does not support mp4.
+			</video>
+			<h3><a href="https://github.com/oscarcoghlan/automata-gl">automata-gl</a></h3>
+			<p>Cellular automata <span class="collapsingtext">(video shows the "maze" ruleset)</span> are a beautiful example of the ways that simple rulesets can create complex results. This project helped me wrap my head around c concepts such as pointers and arrays, and was a neat foray into OpenGL programming, specifically texturing.</p>
+			<p>
+				<a href="https://github.com/oscarcoghlan/automata-gl">&nbsp; > &nbsp; View project on GitHub</a> <br> <br>
+				<a href="/res/automata-gl.exe">&nbsp; > &nbsp; Download standalone Windows PE binary</a>
+			</p>
+			<h2>&nbsp;</h2>
+			<video class="portfolioimage" style="margin-right: 0px; margin-left:22px; float:left;" controls loop>
+				<source src="/media/ballsprev.mp4" type="video/mp4">
+				<p>Preview of ball simulation</p>
+				Your browser does not support mp4.
+			</video>
+			<h3 style="float:right;"><a href="https://github.com/oscarcoghlan/collisions-cpp">collisions-cpp</a></h3>
+			<p style="float:right; text-align:left">Simulations are, frankly, very cool, one of these cool simulations is the simulation of collisions between particles. For now it's just circles, a fully fleged physics engine is, for now, out of my reach. This project taught me about various c++ std library functions such as vectors and objects.</p>
+			<p>
+				<a href="https://github.com/oscarcoghlan/collisions-cpp">&nbsp; > &nbsp; View project on GitHub</a> <br> <br>
+				<a href="/res/collisions-cpp.exe">&nbsp; > &nbsp; Download standalone Windows PE binary</a>
+			</p>
+			<h2>&nbsp;</h2>
+			<video class="portfolioimage" controls loop>
+				<source src="/media/snakeprev.mp4" type="video/mp4">
+				<p>Preview of snake game</p>
+				Your browser does not support mp4.
+			</video>
+			<h3><a href="https://github.com/oscarcoghlan/Java-snake">Java-snake</a></h3>
+			<p>Hugely popular for causual play, the early computer game 'snake' can be considered a veritable behemoth of a social phenomenon. Java is another behemoth of our culture, and it's easy to see why. Programming in Java was a suprisingly pleasant experience - I still love c++ though.</p>
+			<p>
+				<a href="https://github.com/oscarcoghlan/Java-snake">&nbsp; > &nbsp; View project on GitHub</a> <br> <br>
+				<a href="/res/Java-snake.jar">&nbsp; > &nbsp; Download executable .jar file</a>
+			</p>
+		</div>
+		<div class="divwhitespace78" id="Contact"> &nbsp; </div>
+		<h2 class="h2title">Contact Me:</h2>
+		<p>
+		Thank you for visiting my portfolio! If you have any inquiries or would like to connect with me, please feel free to reach out through any of the following channels:
+		<br>
+		<ul>
+			<li><u><a href="mailto:oscar.coghlan@gmail.com">Email</a></u><br></li>
+		<li><u><a href="https://www.linkedin.com/in/oscar-coghlan-b474a1250/">LinkedIn</a></u><br></li>
+		<li><u><a href="https://github.com/oscarcoghlan">GitHub</a></u><br></li>
+		</ul>
+		</p>
 </div>
+	</span>
+	</div>
+	</div>
