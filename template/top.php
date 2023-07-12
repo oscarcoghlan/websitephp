bfc7686ff34e49dec7a35585dc15f2a813d0d706
Oscar Coghlan <oscar.coghlan@gmail.com>
2023-07-12 17:40:52 +1000

    change resume link to search from document root

--- template/top.php
+++ template/top.php
@@ -14,7 +14,7 @@ echo("<title>" . $TITLE . "</title>");
 		<link rel="icon" href="/img/favicon.ico" />
 		<meta charset="UTF-8">
 		<meta name="keywords" content="Oscar Coghlan, Intern, Software Development, Student, Looking for work, University of Canberra, UC, Oscar, Coghlan, Batchelor of Software Engineering, Batchleor of Software Engineering (560AA.7), Internships, Web Development, Full stack">
-		<meta name="description" content="Oscar Coghlan, Software Engineering student at the University of Canberra.">
+		<meta name="description" content="I'm Oscar, a dedicated software engineering student focused on delivering innovative solutions. On this portfolio website, I share my skills, projects, and experiences. I am currently completing a degree in software engineering, and have developed a strong foundation in programming languages, data structures, algorithms, and software design principles. I am actively seeking meaningful opportunities to apply my skills, expand my expertise as a software engineer. ">
 		<meta name="author" content="Oscar Coghlan">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
