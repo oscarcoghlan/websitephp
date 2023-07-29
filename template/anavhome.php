d681c9e9b5e76e52d36d662e246e1819ee1f667d
Oscar Coghlan <oscar.coghlan@gmail.com>
2023-07-29 10:40:50 +1000

    what is template

--- template/anavhome.php
+++
@@ -1,4 +0,0 @@
-<a href="#About">About</a>
-<a href="#Projects">Projects</a>
-<a href="#Contact">Contact</a>
-<span class="spacer">|</span>
--- template/end.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-	<!--<span class="footeritems"><a href=mailto:oscar.coghlan@gmail.com>oscar.coghlan@gmail.com</a> &nbsp; | &nbsp; 2023 &nbsp; | &nbsp; <?php include($BASE_PATH."php/unique_count.php")?></span>-->
-</div>
-</div>
-</div>
-</body>
-</html>
